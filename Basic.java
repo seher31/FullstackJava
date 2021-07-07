cacccbbc9a59c20d3ac888d2d0b89be7046578f9
Seher (inspiron) <sehershah62@gmail.com>
2021-07-07 11:43:30 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,19 @@
+
+public class Basic {
+
+	public static void main(String[] args) {
+      int i=100;
+      char mychar='g';
+      float f=1.2f; //32 bit
+      double d=23.34; //64bit
+      String str="hello world";
+      int j=200;
+      
+      //logic arithmetic operation
+      int sum=i+j;
+      
+      System.out.println("the sum of is..."+sum);
+      
+	}
+
+}
